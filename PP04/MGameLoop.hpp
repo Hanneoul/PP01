4e29fd725a3476ee4d1ade06f5f0c3213cd3793c
DESKTOP-CLMUJNV\elfle <elflee77@gmail.com>
2022-04-03 23:30:43 +0900

    [ES] Monday

--- PP04/MGameLoop.hpp
+++ PP04/MGameLoop.hpp
@@ -8,10 +8,48 @@ using namespace std;
 
 namespace MuSeoun_Engine
 {
+	void MoveCursor(short x, short y)
+	{
+		COORD position = { x , y };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+	}
+
+	void SetCursorState(bool visible)
+	{
+		CONSOLE_CURSOR_INFO consoleCursorInfo;
+		consoleCursorInfo.bVisible = visible;
+		consoleCursorInfo.dwSize = 1;
+		SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &consoleCursorInfo);
+	}
+
+	struct Transform
+	{
+		int x, y;
+	};
+
+	class Player
+	{
+	public:
+		Transform transform;
+
+		Player()
+		{
+			transform.x = 10;
+			transform.y = 15;
+		}
+
+		void draw()
+		{
+			MoveCursor(transform.x, transform.y);
+			cout << "P";
+		}
+	};
+
 	class MGameLoop
 	{
 	private :
-		bool _isGameRunning;		
+		bool _isGameRunning;	
+		Player *p;
 
 	public :
 		MGameLoop() 
@@ -45,17 +83,22 @@ namespace MuSeoun_Engine
 		void Initialize()
 		{
 			SetCursorState(false);
+			p = new Player();
+		}
+		void Release() 
+		{
+			delete(p);
 		}
 
 		void Input()
 		{
-			if (GetAsyncKeyState(VK_SPACE) == -0x8000 || GetAsyncKeyState(VK_SPACE) == -0x8001)
+			if (GetAsyncKeyState(VK_SPACE) & 0x8000 || GetAsyncKeyState(VK_SPACE) & 0x8001)
 			{ 
-				
+				p->transform.y = 10;
 			}
 			else 
 			{
-				
+				p->transform.y = 15;
 			}
 
 		}
@@ -66,35 +109,27 @@ namespace MuSeoun_Engine
 		void Render()
 		{		
 			chrono::system_clock::time_point startRenderTimePoint = chrono::system_clock::now();
-			//system("cls");
-			cout << "Rendering...";
+			system("cls");
+			//cout << "Rendering...";
 			chrono::duration<double> renderDuration = chrono::system_clock::now() - startRenderTimePoint;
 			
+			p->draw();
+			
+			MoveCursor(10, 20);
 			cout << "Rendering speed : " << renderDuration.count() << "sec" << endl;
 
 			int remainingFrameTime = 100 - (int)(renderDuration.count() * 1000.0);
-						
 			if (remainingFrameTime > 0)
 				this_thread::sleep_for(chrono::milliseconds(remainingFrameTime));
 		}
 
-		void Release(){}
-
-	private: //게임 사용 함수
-
-		void MoveCursor(short x, short y)
-		{
-			COORD position = { x , y };
-			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
-		}
-
-		void SetCursorState(bool visible)
-		{
-			CONSOLE_CURSOR_INFO consoleCursorInfo;
-			consoleCursorInfo.bVisible = visible;
-			consoleCursorInfo.dwSize = 1;
-			SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &consoleCursorInfo);
-		}
+				
 		
 	};
+
+	
+
+
+	
+
 }
