e119804329b567b87a31b38ffb267461c14400e4
YUHAN <YUHAN@PC7207>
2022-04-26 15:42:40 +0900

    .

--- PP06/main.cpp
+++ PP06/main.cpp
@@ -1,35 +1,29 @@
-#include <vector> 
+#include <vector>
 #include <iostream>
 
 using namespace std;
-template <typename T, typename C>
-C sum(T a, T b)
-{
-	return a + b;
-}
 
-template <typename T>
+template<typename T>
 T sum(T a, T b)
 {
 	return a + b;
 }
 
+
 int main()
 {
 	vector<int> a;
 
-	cout << sum<double, int>(5.0, 3.434)<<endl;
+	cout << sum<double>(3.9, 4.9) << endl;
+
+	cout << a.size() <<endl;
 
 	a.push_back(1);
 	a.push_back(2);
-	cout << a.size() << endl;
-
 	a.push_back(4);
-	a.push_back(8);
-	a.push_back(16);
-	cout << a.size() << endl;
 
-	cout << a[4] << endl;
+	cout << a.size() << endl;
+	cout << "°ª :"<< a[0] << endl;
 
 	return 0;
 }
